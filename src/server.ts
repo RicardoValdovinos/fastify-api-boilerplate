0e75da49e12560e541bec6972e51e5ed49898c87
Ricardo Valdovinos <ricardovaldovinos1996@gmail.com>
2023-08-10 23:28:03 -0700

    chore: esm glue

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+void server.register(import("./modules/example/route.js"));
+
+server.listen({ port: 3000 }, (error, address) => {
+  if (error) {
+    throw new Error(`Error starting server: ${error.message}`);
+  }
+  console.log(`Server listening at ${address}`);
+});
