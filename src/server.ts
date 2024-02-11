fa4e355261f6eae755dab9e0c2c06112052d1c27
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-02-10 22:28:39 -0700

    feat(server.ts): added closeWithGrace functionality

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import fastify from "fastify";
+import closeWithGrace from "close-with-grace";
+import { exampleRoutes } from "./modules/example/plugins/routes.js";
 
 const server = fastify();
 
-void server.register(import("./modules/example/route.js"));
+void server.register(exampleRoutes);
 
 server.listen({ port: 3000 }, (error, address) => {
-  if (error) {
-    throw new Error(`Error starting server: ${error.message}`);
-  }
-  console.log(`Server listening at ${address}`);
+	if (error) {
+		console.error(`Error starting server: ${error.message}`);
+	}
+
+	console.log(`Server listening at ${address}`);
+});
+
+closeWithGrace({ delay: 500 }, async function ({ signal, err, manual }) {
+	if (err) {
+		console.error(err);
+	}
+
+	console.log(`closeWithGrace: signal=${signal}, manual=${manual}`);
+	await server.close();
 });
