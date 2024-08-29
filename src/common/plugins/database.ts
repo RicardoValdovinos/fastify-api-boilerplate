9aa43dff00b6e0de858710632c20a97744eb69f6
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2024-08-28 19:36:46 -0700

    fix: added missing sql decorator to fastify instance

--- src/common/plugins/database.ts
+++ src/common/plugins/database.ts
@@ -2,12 +2,14 @@ import type { FastifyPluginAsyncTypebox } from "@fastify/type-provider-typebox";
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 import fp from "fastify-plugin";
 import { kysely } from "../../configs/database.js";
+import { sql } from "kysely";
 
 const database: FastifyPluginAsyncTypebox = async (
 	instance: FastifyInstance,
 	_options: FastifyPluginOptions
 ): Promise<void> => {
 	instance.decorate("database", kysely);
+	instance.decorate("sql", sql);
 };
 
 export default fp(database, {
